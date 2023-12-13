b6dcbd519dc820bc166df864167a587feb0b8ac1
Rodo-ASUS <rununez@entel.cl>
2023-12-13 19:28:19 -0300

    a01 creado

---
+++ preprocessing/a01_preproceso.py
@@ -0,0 +1,27 @@
+# Librerias ---------------------------------------- 
+
+import pandas as pd
+
+# Loading data ---------------------------------------- 
+users_behavior_raw = pd.read_csv('files/datasets/input/users_behavior.csv')
+
+# Cleaning columns ---------------------------------------- 
+
+def limpiar_columnas(dataset):
+                return dataset
+
+users_behavior = limpiar_columnas(users_behavior_raw)
+
+# Eliminating duplicates per period ---------------------------------------- 
+
+# ...
+
+# Checking NAs ---------------------------------------- 
+
+# ...
+
+# Guardar datos ---------------------------------------- 
+
+users_behavior.to_csv("files/datasets/intermediate/a01_users_behavior_cleaned.csv")
+
+
