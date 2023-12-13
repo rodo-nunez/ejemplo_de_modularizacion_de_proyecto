e23f37002c40ad1cee560f6ac22f622f10d6d04b
Rodo-ASUS <rununez@entel.cl>
2023-12-13 20:25:47 -0300

    aplicacion de funcion crear_formatos_fecha

--- params.py
+++ params.py
@@ -1,6 +1,7 @@
 # Librerias ---------------------------------------- 
 
 import platform 
+from functions.crear_formatos_fecha import *
 
 # Systema operativo ---------------------------------------- 
 
@@ -12,4 +13,5 @@ bool_entrtenamiento = False
 
 # Fecha por defecto ---------------------------------------- 
 
-periodo_YYYYMM = "202311"
+periodo_YYYYMM_por_defecto = "202311"
+periodo_YYYYMM, anio_mes_dia_formato_ini_date, anio_mes_dia_formato_ini, anio_mes_dia_formato_fin = crear_formatos_fecha(periodo_YYYYMM_por_defecto)
