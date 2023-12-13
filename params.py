8e94d9b91fbd35b4ad14b0d7226d14ae696d84b3
Rodo-ASUS <rununez@entel.cl>
2023-12-13 20:16:29 -0300

    agregado periodo de ejecucion

--- params.py
+++ params.py
@@ -9,3 +9,7 @@ sistema_operativo = platform.system()
 # Entrenamiento o ejecucion ---------------------------------------- 
 
 bool_entrtenamiento = False
+
+# Fecha por defecto ---------------------------------------- 
+
+periodo_YYYYMM = "202311"
