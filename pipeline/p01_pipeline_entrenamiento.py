8426cc7a2a41ba363a8cd2943d633bb996e0f426
Rodo-ASUS <rununez@entel.cl>
2023-12-13 20:12:09 -0300

    pipeline basico funcionando

--- pipeline/p01_pipeline_entrenamiento.py
+++ pipeline/p01_pipeline_entrenamiento.py
@@ -2,15 +2,23 @@
 
 import os, sys
 import argparse
+import params as params
+
+# Definir extension de ejecutables ---------------------------------------- 
+
+if params.sistema_operativo == 'Windows':
+        extension_binarios = ".exe"
+else:
+        extension_binarios = ""
 
 # Preproceso ---------------------------------------- 
 
-os.system(f"preprocessing/a01_preproceso.py")
+os.system(f"python{extension_binarios} preprocessing/a01_preproceso.py")
 
-os.system(f"preprocessing/a02_escalador.py")
+os.system(f"python{extension_binarios} preprocessing/a02_escalador.py")
 
-os.system(f"preprocessing/a03_split_train_test.py")
+os.system(f"python{extension_binarios} preprocessing/a03_split_train_test.py")
 
 # Modelo ---------------------------------------- 
 
-os.system(f"models/b01_creacion_de_modelos.py")
+os.system(f"python{extension_binarios} models/b01_creacion_de_modelos.py")
